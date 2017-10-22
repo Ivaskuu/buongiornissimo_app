ddd44653a4c228ac70d4fa6c05581bde5d7fbcd2
Ivaskuu <ivascuadrian9+github@gmail.com>
2017-10-22 15:59:34 +0200

    Implemented the AppBar in the MainUI

--- lib/main_ui.dart
+++ lib/main_ui.dart
@@ -1,1 +1,40 @@
-im
+import 'package:flutter/material.dart';
+
+Color caffeColor = new Color.fromRGBO(92, 75, 81, 1.0);
+
+class MainUI extends StatefulWidget
+{
+  @override
+  MainUIState createState() => new MainUIState();
+}
+
+class MainUIState extends State<MainUI>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return new Scaffold
+    (
+      appBar: new AppBar
+      (
+        backgroundColor: caffeColor,
+        elevation: 0.0,
+        leading: new Icon(Icons.local_cafe, color: Colors.white),
+        title: new Text("Buongiornissimo", style: new TextStyle(color: Colors.white)),
+        actions: <Widget>
+        [
+          new CircleAvatar
+          (
+            backgroundColor: Colors.white,
+            child: new Icon(Icons.account_circle, color: caffeColor),
+          )
+        ]
+      ),
+      backgroundColor: caffeColor,
+      body: new Center
+      (
+        child: new Text("Kaffee ?"),
+      ),
+    );
+  }
+}
