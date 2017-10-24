e91baaf61a066015dde6bf0ab4642fde147442a6
Ivaskuu <ivascuadrian9+github@gmail.com>
2017-10-24 22:10:28 +0200

    Added a popup on oggetto click

--- lib/ordina_ui.dart
+++ lib/ordina_ui.dart
@@ -49,18 +49,18 @@ class OrdinaUIState extends State<OrdinaUI> with SingleTickerProviderStateMixin
         controller: controller,
         children: <Widget>
         [
-          gridList([
+          gridList(context, [
             new Oggetto("Caffè Espresso", 0.25, 0, "http://www.fondazionebellaria.it/wp-content/uploads/2014/09/CAFFE-MEMORIA.jpg"),
             new Oggetto("Caffè Lungo", 0.25, 0, "http://www.fondazionebellaria.it/wp-content/uploads/2014/09/CAFFE-MEMORIA.jpg"),
             new Oggetto("Caffè Macchiato", 0.25, 0, "http://www.fondazionebellaria.it/wp-content/uploads/2014/09/CAFFE-MEMORIA.jpg"),
             new Oggetto("Cioccolata", 0.25, 0, "http://www.costaricanewtravel.com/wp-content/uploads/2015/03/cioccolato-fuso-costa-rica-4jpg.jpg"),
           ]),
-          gridList([
+          gridList(context, [
             new Oggetto("Acqua naturale", 0.30, 1, "http://www.veloxsrl.it/wp-content/uploads/2015/09/san-benedetto-naturale-1-5-foto.jpg"),
             new Oggetto("Acqua frizzante", 0.30, 1, "http://www.staples.it/content/images/product/34706_1_xnl.jpg"),
             new Oggetto("Pepsi", 0.70, 1, "https://www.grow-shop-italia.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/-/1-stash-lattina-pepsi-head-shop-gsi.jpg"),
           ]),
-          gridList([
+          gridList(context, [
             new Oggetto("Croccantelle Ketchup", 0.40, 2, "http://www.fornodamiani.it/wp-content/uploads/2015/04/ing_CROCCANTELLE_ketchup1.jpg"),
             new Oggetto("Croccantelle Prosciutto", 0.40, 2, "https://images-na.ssl-images-amazon.com/images/I/61%2BLnxjkcZL._SX522_.jpg"),
             new Oggetto("Croccantelle Formaggio", 0.40, 2, "http://www.goccedespressione.it/390-thickbox_default/croccantelle-al-bacon-forno-damiani.jpg"),
@@ -70,7 +70,7 @@ class OrdinaUIState extends State<OrdinaUI> with SingleTickerProviderStateMixin
             new Oggetto("M&M's", 0.75, 2, "https://pics.drugstore.com/prodimg/318313/450.jpg"),
             new Oggetto("Twix", 0.75, 2, "https://www.poundstretcher.co.uk/media/catalog/product/cache/1/image/686x/9df78eab33525d08d6e5fb8d27136e95/2/9/292114-twix_50gm.jpg"),
           ]),
-          gridList([
+          gridList(context, [
             new Oggetto("Panino Cotoletta", 2.00, 0, "http://www.igusto.it/98-thickbox_default/Panino-Cotoletta.jpg"),
             new Oggetto("Panino Cordon Bleu", 2.00, 0, "https://foodblogs-sympledesignllc.netdna-ssl.com/OSqzTpniBAzFVaLhcAjW5lxte9U=/800x800/smart/www.fifteenspatulas.com%2Fwp-content%2Fuploads%2F2015%2F01%2FChicken_Cordon_Bleu_Panini_Recipe_fifteenspatulas_10.jpg"),
             new Oggetto("Panino salame", 1.30, 0, "http://www.consapienza.it/wordpress/wp-content/uploads/blogger/-4pDy0Kop_ek/UIVRBkEa1eI/AAAAAAAABVE/JdGtfU_T_fU/s1600/panino%2Bal%2Blatte_salame.png"),
@@ -83,7 +83,7 @@ class OrdinaUIState extends State<OrdinaUI> with SingleTickerProviderStateMixin
     );
   }
 
-  Widget gridList(List<Oggetto> oggetti)
+  Widget gridList(var context, List<Oggetto> oggetti)
   {
     return new Container
     (
@@ -96,13 +96,13 @@ class OrdinaUIState extends State<OrdinaUI> with SingleTickerProviderStateMixin
           mainAxisSpacing: 5.0,
           crossAxisSpacing: 5.0
         ),
-        itemBuilder: (_, int pos) => oggettoUI(oggetti[pos]),
+        itemBuilder: (_, int pos) => oggettoUI(context, oggetti[pos]),
         itemCount: oggetti.length,
       )
     );
   }
   
-  Widget oggettoUI(Oggetto oggetto)
+  Widget oggettoUI(var context, Oggetto _oggetto)
   {
     return new GridTile
     (
@@ -110,18 +110,54 @@ class OrdinaUIState extends State<OrdinaUI> with SingleTickerProviderStateMixin
       (
         child: new InkWell
         (
-          onTap: () => null,
+          onTap: () => _showOggettoDialog(context, _oggetto),
           child: new Container
           (
-            decoration: new BoxDecoration(image: new DecorationImage(image: new NetworkImage(oggetto.image), fit: BoxFit.cover)),
+            decoration: new BoxDecoration(image: new DecorationImage(image: new NetworkImage(_oggetto.image), fit: BoxFit.cover)),
           ),
         ),
       ),
       footer: new GridTileBar
       (
         backgroundColor: Colors.black87,
-        title: new Text(oggetto.nome),
-        subtitle: new Text("${oggetto.prezzo.toStringAsFixed(2)} €"),
+        title: new Text(_oggetto.nome),
+        subtitle: new Text("${_oggetto.prezzo.toStringAsFixed(2)} €"),
+      ),
+    );
+  }
+
+  void _showOggettoDialog(var context, Oggetto _oggetto)
+  {
+    showDialog
+    (
+      context: context,
+      barrierDismissible: true,
+      child: new SimpleDialog
+      (
+        children: <Widget>
+        [
+          new SizedBox.fromSize
+          (
+            size: new Size.fromHeight(200.0),
+            child: new Container(decoration: new BoxDecoration(image: new DecorationImage(image: new NetworkImage(_oggetto.image), fit: BoxFit.cover))),
+          ),
+          new ListTile
+          (
+            title: new Text(_oggetto.nome),
+            subtitle: new Text("${_oggetto.prezzo.toStringAsFixed(2)} €"),
+            trailing: new Text("53 kcal"),
+          ),
+          new Container
+          (
+            margin: new EdgeInsets.symmetric(horizontal: 8.0),
+            child: new RaisedButton
+            (
+              onPressed: () => null,
+              child: new Text("AGGIUNGI AL CARELLO (2)", style: new TextStyle(color: Colors.white)),
+              color: colorCaffe,
+            )
+          )
+        ],
       ),
     );
   }
